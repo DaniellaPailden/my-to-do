ca6229c50860acb34987e2749a7a340d5a3574f6
Daniella <daniellalpailden@gmail.com>
2025-05-09 19:47:53 +0800

    [update] configuring task service, and json server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { TasksComponent } from './components/tasks/tasks.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TasksComponent],
+  imports: [HeaderComponent, TasksComponent, CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-to-do';
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http'; 
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(),
-    provideAnimations(),
-    // provideRouter(appRoutes)
-  ] 
+    provideHttpClient(), // Ensure HttpClient is provided
+    provideAnimations()
+  ]
 }).catch(err => console.error(err));
